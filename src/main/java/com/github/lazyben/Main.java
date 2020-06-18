c20561dc34a50f7ab3d20b33fe0388b5e2b13a81
lazyben <458861463@qq.com>
2020-06-18 11:11:09 +0800

    项目初始化

---
+++ src/main/java/com/github/lazyben/Main.java
@@ -0,0 +1,4 @@
+package com.github.lazyben;
+
+public class Main {
+}
---
+++ src/test/java/com/github/lazyben/CrawlerTest.java
@@ -0,0 +1,15 @@
+package com.github.lazyben;
+
+import com.opencsv.CSVReader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GitHub;
+
+public class CrawlerTest {
+
+}
